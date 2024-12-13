e29a00226377149c7b5827c8340e61393cdd17ee
mihateren <mihateren2003@gmail.com>
2024-12-13 18:08:47 +0300

    fix separator, section width

--- src/components/(shared)/landing/landingPage.tsx
+++ src/components/(shared)/landing/landingPage.tsx
@@ -1,27 +1,32 @@
 import LandingCarousel from "./landingCarousel"
-import ThemeToggle from "@/providers/items/themeToggle";
+import CustomSeparator from "@/components/(shared)/common/customSeparator";
 
 export default function LandingPage() {
 	return (
-		<section className="h-screen flex flex-row justify-center items-center gap-x-10">
-			<div className="w-1/2 flex flex-col justify-center space-y-6 relative">
-				<h1 className="font-semibold text-4xl leading-tight">
-					Слоган компании, который мы еще не придумали
-				</h1>
-				<div>
-					<div className="relative w-full">
-						<div className="w-[50%] border-t border-gray-800"></div>
-						<div className="absolute right-[50%] top-1/2 -translate-y-1/2 rotate-45 w-4 h-4 bg-gray-800"></div>
-					</div>
+		<>
+			<section className="h-screen flex flex-row justify-center items-center gap-x-10">
+				<div className="w-full flex flex-col justify-center space-y-6 relative">
+					<h1 className="font-semibold text-5xl leading-tight">
+						Слоган компании, который мы еще не придумали
+					</h1>
+					<CustomSeparator/>
+					<p className="pt-20 text-xl leading-relaxed">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eleifend, nulla ut posuere maximus, diam eros
+						mattis dui, ac rhoncus urna lorem elementum lorem.
+					</p>
 				</div>
-				<p className="pt-20 text-xl leading-relaxed">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eleifend, nulla ut posuere maximus, diam eros
-					mattis dui, ac rhoncus urna lorem elementum lorem.
-				</p>
-			</div>
-			<div>
-				<LandingCarousel />
-			</div>
-		</section>
+				<div>
+					<LandingCarousel/>
+				</div>
+			</section>
+			<section className="flex flex-col items-center w-full">
+				<div className="flex flex-row justify-between w-full mx-auto gap-x-10">
+					<CustomSeparator direction="left" className="my-8 flex-1"/>
+					<CustomSeparator direction="left" className="my-8 flex-1"/>
+				</div>
+			</section>
+
+
+		</>
 	);
 }
