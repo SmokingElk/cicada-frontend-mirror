957ddd057ca81244bf81f7aac1079ab38367e15b
Вася <vasyamontenegro1985@gmail.com>
2025-02-28 10:03:18 +0300

    Added gameNav

--- src/components/(shared)/game/gameNav/gameNav.tsx
+++ src/components/(shared)/game/gameNav/gameNav.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import { Styleable } from "@/lib/types";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Wrapper from "@/components/(shared)/common/wrapper";
+import { ReactNode } from "react";
 
 export default function GameNav({ className = "" }: Styleable) {
-  return <div className={className}></div>;
+  return (
+        <div className="w-full flex flex-col justify-end space-y-6">
+            <Button className="w-full h-16" variant="transparrent" size="xl" asChild>
+              <Link href="/game">Ничья</Link>
+            </Button>
+            <Button className="w-full h-16" variant="transparrent" size="xl" asChild>
+                <Link href="/game">Сдаться</Link>
+            </Button>
+        </div>
+  );
 }
