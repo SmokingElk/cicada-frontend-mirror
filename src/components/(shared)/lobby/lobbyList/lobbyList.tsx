5b749c1093f612a93ba5835402009548beca89ab
VaasMontenegro1985 <annabacach1602@gmail.com>
2025-03-16 20:44:17 +0300

    Added lobbyList and lobbySearch

--- src/components/(shared)/lobby/lobbyList/lobbyList.tsx
+++ src/components/(shared)/lobby/lobbyList/lobbyList.tsx
@@ -1,6 +1,69 @@
-import { Styleable } from "@/lib/types";
-import { cn } from "@/lib/utils";
+"use client";
 
-export default function LobbyList({ className = "" }: Styleable) {
-  return <div className={cn("w-full", className)}></div>;
+import {Styleable} from "@/lib/types";
+import {cn} from "@/lib/utils";
+import DecoratedTile from "@/components/(shared)/common/decoratedTile";
+
+export default function LobbyList({className = ""}: Styleable) {
+    // Статические данные
+    const parties = [
+        {
+            partyName: "Мат в 7 ходов",
+            opponentName: "PlayerName",
+            time: "3:00",
+            rating: "100",
+            href: "/",
+            imageUrl: "/avatars/avatarPlaceholder.png"
+        },
+        {
+            partyName: "Унижаю новичков",
+            opponentName: "IvanIvanich",
+            time: "3:00",
+            rating: "1000",
+            href: "/",
+            imageUrl: "/avatars/avatarOpponentPlaceholder.png"
+        },
+        {
+            partyName: "Я новичок",
+            opponentName: "KingOfWorld",
+            time: "10:00",
+            rating: "0",
+            href: "/",
+            imageUrl: "/avatars/avatarPlaceholder.png"
+        },
+    ];
+
+    return (
+        <div className={cn("w-full flex flex-col min-h-screen justify-start", className)}>
+            <div
+                className="font-montserrat text-foreground text-4xl outline-none font-semibold pt-6 pb-12 placeholder-foreground">
+                Ожидают соперника:
+            </div>
+            <ul className="space-y-4 pr-4">
+                {parties.map((party, index) => (
+                    <li key={index}>
+                        <div className="w-full">
+                            <DecoratedTile className="text-background min-w-full">
+                                <div className="flex flex-col w-[550px] p-2 pl-3">
+                                    <div className="flex flex-row pb-1">
+                                        <span className="flex-start">{party.partyName}</span>
+                                        <span className="ml-auto">{party.time}</span>
+                                    </div>
+                                    <hr className="border-t border-gray-300 py-1 h-1 w-full"/>
+                                    <div className="flex flex-row-1/2">
+                                        <img
+                                            src={party.imageUrl} // Ссылка на изображение
+                                            className="w-7 h-7 rounded-full mr-2" // Стили для фото
+                                        />
+                                        <span className="flex-start">{party.opponentName}</span>
+                                        <span className="ml-auto">Рейтинг: {party.rating}</span>
+                                    </div>
+                                </div>
+                            </DecoratedTile>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
--- src/components/(shared)/lobby/lobbySearch/lobbySearch.tsx
+++ src/components/(shared)/lobby/lobbySearch/lobbySearch.tsx
@@ -1,6 +1,25 @@
-import { Styleable } from "@/lib/types";
-import { cn } from "@/lib/utils";
+"use client";
 
-export default function LobbySearch({ className = "" }: Styleable) {
-  return <div className={cn("w-full", className)}></div>;
+import {Styleable} from "@/lib/types";
+import {cn} from "@/lib/utils";
+import {useState} from "react";
+
+export default function LobbySearch({className = ""}: Styleable) {
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    return (
+        <div className={cn("w-full flex flex-row", className)}>
+            <form action="/search" method="get" className="flex flex-row items-center w-full">
+                <input
+                    className="font-montserrat text-foreground text-l italic flex-grow bg-transparent outline-none font-semibold h-[70px] pl-1 placeholder-foreground"
+                    placeholder="Введите имя игрока или партии..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="flex-shrink-0 pr-4">
+                    <img className="w-7 h-7" src="/icons/loupe.svg" alt="Search"/> {}
+                </button>
+            </form>
+        </div>
+    );
 }
