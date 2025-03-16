749415a4f0c996495aa7ab78d299102f4f854c05
SmokingElk <andre12go@mail.ru>
2025-03-16 13:26:53 +0300

    added lobby sidebar

--- src/components/(shared)/lobby/lobbyFilters/lobbyFilters.tsx
+++ src/components/(shared)/lobby/lobbyFilters/lobbyFilters.tsx
@@ -1,6 +1,49 @@
+"use client";
+
+import { SliderDouble } from "@/components/ui/slider-double";
 import { Styleable } from "@/lib/types";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export default function LobbyFilters({ className = "" }: Styleable) {
-  return <div className={cn("w-full", className)}></div>;
+  const rateMin = 0;
+  const rateMax = 1000;
+
+  const [rateRange, setRateRange] = useState(
+    [
+      rateMin + (rateMax - rateMin) / 3,
+      rateMin + ((rateMax - rateMin) / 3) * 2,
+    ].map(Math.round)
+  );
+
+  return (
+    <div className={cn("w-full", className)}>
+      <div className="w-full flex justify-center items-center font-montserrat font-semibold text-foreground text-2xl h-20 mb-8">
+        Фильтры
+      </div>
+
+      <div className="w-full">
+        <div className="font-roboto italic text-foreground text-lg mb-3">
+          Диапазон рейтинга противника:
+        </div>
+
+        <div className="flex">
+          <div className="w-20 flex justify-center font-roboto-text-foreground">
+            {rateRange[0]}
+          </div>
+          <SliderDouble
+            min={rateMin}
+            max={rateMax}
+            value={rateRange}
+            onValueChange={(newValues) => {
+              setRateRange(newValues.map(Math.round));
+            }}
+          />
+          <div className="w-20 flex justify-center font-roboto-text-foreground">
+            {rateRange[1]}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
