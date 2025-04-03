45781d0a24f2cfbed4c53a8bb1ab3232bc3789fd
VaasMontenegro1985 <annabacach1602@gmail.com>
2025-04-03 11:25:33 +0300

    Added adaptive for landing and profile

--- src/components/(shared)/framing/header.tsx
+++ src/components/(shared)/framing/header.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Image from "next/image"
-import logo from "@/../public/logo.svg"
+import logoLight from "@/../public/icons/lightTheme/logoLight.svg"
+import logoDark from "@/../public/icons/darkTheme/logoDark.svg"
 import Wrapper from "@/components/(shared)/common/wrapper"
 
 import HeaderMenu from "@/components/(shared)/framing/headerMenu";
+import {useTheme} from "next-themes";
+
 
 export default function Header() {
+	const {theme} = useTheme();
+	const logo = theme === "dark" ? logoDark : logoLight;
 	return (
-		<header className="fixed top-0 left-0 right-0 h-[var(--header-height)] z-40 w-full mx-auto bg-background">
+		<header className="fixed top-0 left-0 right-0 h-[var(--header-height)] z-40 w-full bg-background">
 			<Wrapper className="flex flex-row items-center justify-between py-4">
 				<div className="flex items-center">
-					<Image src={logo} alt="Logo" className="h-1/3 md:h-auto" width={40} height={36}/>
+					<Image className="fg-foreground h-1/3 md:h-auto" src={logo} alt="Logo" width={40} height={36}/>
 					<h1 className="text-2xl ml-5 font-bold">Cicada Chess</h1>
 				</div>
 				<HeaderMenu/>
