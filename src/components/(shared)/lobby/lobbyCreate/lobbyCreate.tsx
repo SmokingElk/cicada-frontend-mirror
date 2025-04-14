15bd18e71af140314e21ebe5dce2322ee13b2465
Вася <vasyamontenegro1985@gmail.com>
2025-04-14 14:18:41 +0300

    adaptive for lobby

--- src/components/(shared)/lobby/lobbyCreate/lobbyCreate.tsx
+++ src/components/(shared)/lobby/lobbyCreate/lobbyCreate.tsx
@@ -11,8 +11,8 @@ import RhombusDecor from "@/components/(shared)/common/rhombusDecor";
 
 const formatTime = (seconds: number) => {
   const mins = Math.floor(seconds / 60)
-    .toString()
-    .padStart(2, "0");
+      .toString()
+      .padStart(2, "0");
   const secs = (seconds % 60).toString().padStart(2, "0");
   return `${mins}:${secs}`;
 };
@@ -23,20 +23,20 @@ interface ColorSelectorProps extends Styleable {
 }
 
 function ColorSelector({
-  selected,
-  setColor,
-  className = "",
-}: ColorSelectorProps) {
+                         selected,
+                         setColor,
+                         className = "",
+                       }: ColorSelectorProps) {
   const center =
-    "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2";
+      "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2";
 
   return (
-    <div className="relative w-6 h-4 cursor-pointer" onClick={setColor}>
-      <RhombusDecor
-        className={cn(center, "border-[3px] size-5 border-primary", className)}
-      />
-      {selected && <RhombusDecor className={cn(center, "size-2 bg-primary")} />}
-    </div>
+      <div className="relative w-6 h-4 cursor-pointer" onClick={setColor}>
+        <RhombusDecor
+            className={cn(center, "border-[3px] size-5 border-primary", className)}
+        />
+        {selected && <RhombusDecor className={cn(center, "size-2 bg-primary")} />}
+      </div>
   );
 }
 
@@ -48,120 +48,117 @@ export default function LobbyCreate({ className = "" }: Styleable) {
 
   const timeMin = 3 * 60;
   const timeMax = 30 * 60;
-
   const timePresets = [3 * 60, 5 * 60, 10 * 60, 30 * 60];
 
   return (
-    <div className={cn("w-full h-full relative overflow-hidden", className)}>
-      <div
-        className={cn(
-          "absolute top-3/4 w-full flex justify-center transition-all duration-700",
-          createOpen ? "-top-1/4" : ""
-        )}
-      >
-        <Button
-          className="w-5/6 h-16 text-2xl"
-          variant="transparrent"
-          size="xl"
-          onClick={() => setCreateOpen(true)}
+      <div className={cn("w-full md:py-4 py-16", className)}>
+        <div
+            className={cn(
+                "w-full transition-all duration-300 overflow-hidden",
+                createOpen ? "max-h-[1000px]" : "max-h-0"
+            )}
         >
-          Создать партию
-        </Button>
-      </div>
-
-      <div
-        className={cn(
-          "absolute top-full w-full auto grid grid-rows-[auto_100px] gap-3 transition-all duration-700",
-          createOpen ? "top-0" : ""
-        )}
-      >
-        <div className="w-full p-8 box-border border-4 border-primary relative">
-          <div
-            className="absolute top-4 right-4 cursor-pointer"
-            onClick={() => setCreateOpen(false)}
-          >
-            <X />
-          </div>
-          <div className="w-full flex justify-center items-center font-montserrat font-semibold text-foreground text-2xl mb-8">
-            Создать игру
-          </div>
-
-          <div className="w-full border-b-2 border-foreground mb-8">
-            <input
-              className={cn(
-                "font-roboto text-foreground text-lg w-full bg-transparent outline-none",
-                "placeholder:italic placeholder:text-foreground placeholder:opacity-50"
-              )}
-              placeholder="Введите имя игры"
-              value={gameName}
-              onChange={(e) => setGameName(e.target.value)}
-            ></input>
-          </div>
-
-          <div className="w-full">
-            <div className="font-roboto italic text-foreground text-lg mb-2">
-              Укажите время на ход
+          <div className="w-full p-8 box-border border-4 border-primary relative mb-4">
+            <div
+                className="absolute top-4 right-4 cursor-pointer"
+                onClick={() => setCreateOpen(false)}
+            >
+              <X />
             </div>
 
-            <div className="font-montserrat font-medium text-foreground text-2xl mb-5">
-              {formatTime(moveTime)}
+            <div className="w-full flex justify-center items-center font-montserrat font-semibold text-foreground text-2xl mb-8">
+              Создать игру
             </div>
 
-            <Slider
-              value={[moveTime]}
-              onValueChange={(e) => setMoveTime(Math.round(e[0]))}
-              min={timeMin}
-              max={timeMax}
-              className="mb-4"
-            />
-
-            <div className="w-full flex justify-between mb-8">
-              {timePresets.map((e, index) => (
-                <div
-                  className="font-montserrat font-medium text-foreground text-xl mb-5 opacity-70 hover:opacity-100 cursor-pointer"
-                  key={`time_preset_${index}`}
-                  onClick={() => setMoveTime(e)}
-                >
-                  {formatTime(e)}
-                </div>
-              ))}
+            <div className="w-full border-b-2 border-foreground mb-8">
+              <input
+                  className={cn(
+                      "font-roboto text-foreground text-lg w-full bg-transparent outline-none",
+                      "placeholder:italic placeholder:text-foreground placeholder:opacity-50"
+                  )}
+                  placeholder="Введите имя игры"
+                  value={gameName}
+                  onChange={(e) => setGameName(e.target.value)}
+              />
             </div>
 
             <div className="w-full">
               <div className="font-roboto italic text-foreground text-lg mb-2">
-                Ваш цвет: {isWhite ? "белый" : "черный"}
+                Укажите время на ход
               </div>
 
-              <div className="flex gap-6">
-                <ColorSelector
-                  setColor={() => setIsWhite(false)}
-                  selected={!isWhite}
-                />
-                <ColorSelector
-                  setColor={() => setIsWhite(true)}
-                  selected={isWhite}
-                  className="bg-background"
-                />
+              <div className="font-montserrat font-medium text-foreground text-xl md:text-2xl mb-5">
+                {formatTime(moveTime)}
+              </div>
+
+              <Slider
+                  value={[moveTime]}
+                  onValueChange={(e) => setMoveTime(Math.round(e[0]))}
+                  min={timeMin}
+                  max={timeMax}
+                  className="mb-4"
+              />
+
+              <div className="w-full flex flex-wrap justify-between gap-2 mb-8">
+                {timePresets.map((e, index) => (
+                    <div
+                        className="font-montserrat font-medium text-foreground text-base md:text-xl opacity-70 hover:opacity-100 cursor-pointer px-1 md:px-2"
+                        key={`time_preset_${index}`}
+                        onClick={() => setMoveTime(e)}
+                    >
+                      {formatTime(e)}
+                    </div>
+                ))}
+              </div>
+
+              <div className="w-full">
+                <div className="font-roboto italic text-foreground text-lg mb-2">
+                  Ваш цвет: {isWhite ? "белый" : "черный"}
+                </div>
+
+                <div className="flex gap-6">
+                  <ColorSelector
+                      setColor={() => setIsWhite(false)}
+                      selected={!isWhite}
+                  />
+                  <ColorSelector
+                      setColor={() => setIsWhite(true)}
+                      selected={isWhite}
+                      className="bg-background"
+                  />
+                </div>
               </div>
             </div>
           </div>
+
+          <div className="w-full mb-4">
+            <Button
+                className="bg-foreground w-full h-16 rounded-none hover:bg-primary"
+                asChild
+            >
+              <Link href="/game" className="flex justify-center w-full gap-4">
+                <RhombusDecor className="bg-primary size-5" />
+                <div className="font-montserrat font-semibold text-background text-2xl">
+                  Готово
+                </div>
+                <RhombusDecor className="bg-primary size-5" />
+              </Link>
+            </Button>
+          </div>
         </div>
 
-        <div className="w-full h-full">
-          <Button
-            className="bg-foreground w-full h-full rounded-none hover:bg-primary"
-            asChild
-          >
-            <Link href="/game" className="flex justify-center w-full gap-4">
-              <RhombusDecor className="bg-primary size-5" />
-              <div className="font-montserrat font-semibold text-background text-2xl">
-                Готово
-              </div>
-              <RhombusDecor className="bg-primary size-5" />
-            </Link>
-          </Button>
-        </div>
+        {!createOpen && (
+            <div className="w-full flex justify-center">
+              <Button
+                  className="w-5/6 h-16 text-2xl"
+                  variant="transparrent"
+                  size="xl"
+                  onClick={() => setCreateOpen(true)}
+              >
+                Создать партию
+              </Button>
+            </div>
+        )}
       </div>
-    </div>
   );
 }
--- src/components/(shared)/lobby/lobbyList/lobbyList.tsx
+++ src/components/(shared)/lobby/lobbyList/lobbyList.tsx
@@ -5,7 +5,6 @@ import {cn} from "@/lib/utils";
 import DecoratedTile from "@/components/(shared)/common/decoratedTile";
 
 export default function LobbyList({className = ""}: Styleable) {
-    // Статические данные
     const parties = [
         {
             partyName: "Мат в 7 ходов",
@@ -34,26 +33,26 @@ export default function LobbyList({className = ""}: Styleable) {
     ];
 
     return (
-        <div className={cn("w-full flex flex-col min-h-screen justify-start", className)}>
+        <div className={cn("w-full flex flex-col h-auto justify-start", className)}>
             <div
-                className="font-montserrat text-foreground text-4xl outline-none font-semibold pt-6 pb-12 placeholder-foreground">
+                className="font-montserrat text-foreground text-2xl md:text-4xl outline-none font-semibold pt-6 pb-3 md:pb-12 placeholder-foreground">
                 Ожидают соперника:
             </div>
-            <ul className="space-y-4 pr-4">
+            <ul className="space-y-4 md:pr-4">
                 {parties.map((party, index) => (
                     <li key={index}>
                         <div className="w-full">
-                            <DecoratedTile className="text-background min-w-full">
-                                <div className="flex flex-col w-[550px] p-2 pl-3">
-                                    <div className="flex flex-row pb-1">
+                            <DecoratedTile className="text-sm md:text-base text-background min-w-full">
+                                <div className="flex flex-col w-full max-w-screen-sm md:w-[550px] p-2 pl-3">
+                                    <div className="flex flex-row w-full pb-1">
                                         <span className="flex-start">{party.partyName}</span>
                                         <span className="ml-auto">{party.time}</span>
                                     </div>
                                     <hr className="border-t border-gray-300 py-1 h-1 w-full"/>
-                                    <div className="flex flex-row-1/2">
+                                    <div className="flex flex-row-1/2 w-full">
                                         <img
-                                            src={party.imageUrl} // Ссылка на изображение
-                                            className="w-7 h-7 rounded-full mr-2" // Стили для фото
+                                            src={party.imageUrl}
+                                            className="w-7 h-7 rounded-full mr-2"
                                         />
                                         <span className="flex-start">{party.opponentName}</span>
                                         <span className="ml-auto">Рейтинг: {party.rating}</span>
